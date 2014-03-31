01dd868ec0948128f7994903c7e46476fd15acc3
Jervis Thompson <jervo@mac.com>
2014-03-30 20:54:18 -0400

    Added dynamic day left in course pit chart.

--- build/index.php
+++ build/index.php
@@ -9,7 +9,7 @@
 	}
 </style>
 </head>
-<body>
+<body onload="resize_canvas()">
 
 	<div id="layout" class="pure-g-r">
 		<a href="#menu" id="menuLink" class="pure-menu-link">
@@ -50,12 +50,15 @@
 						<div class="l-hbox">
 							<h2 class="content-subhead">Hello World!</h2>
 							<p>This is the class website for WBDV 240. On this site you will find the course syllabus, calendar, a list of resources, and full lecture notes for all 10 weeks of class as well as links to contact instructors for help.</p>
+							<h2 class="content-subhead">Class Textbook Is Now Available Online</h2>
+							<p>Our <a href="//www.amazon.com/HTML-CSS-Design-Build-Websites/dp/1118008189%3FSubscriptionId%3DAKIAJYKJBAJTAUI7IXAQ%26tag%3Dcoursekit-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D1118008189" title="Required Textbook" target="_blank">required text book</a> (<a href="http://www.amazon.com/HTML-CSS-Design-Build-Websites/dp/1118008189%3FSubscriptionId%3DAKIAJYKJBAJTAUI7IXAQ%26tag%3Dcoursekit-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D1118008189" target="_blank">Amazon</a>) is now available in the <a href="//innoserv.library.drexel.edu/search~S9?/tHTML+and+CSS:+Design+and+Build+Websites/thtml+and+css+design+and+build+websites/1%2C1%2C2%2CB/frameset&amp;FF=thtml+and+css+design+and+build+websites&amp;1%2C%2C2" target="_blank">Haggerty Library electronic resources</a>.</p>
 						</div>
 					</div>
 					<div class="pure-u-1-2">
 						<div class="l-hbox">
-							<h2 class="content-subhead">Class Textbook Is Now Available Online</h2>
-							<p>Our <a href="//www.amazon.com/HTML-CSS-Design-Build-Websites/dp/1118008189%3FSubscriptionId%3DAKIAJYKJBAJTAUI7IXAQ%26tag%3Dcoursekit-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D1118008189" title="Required Textbook" target="_blank">required text book</a> (<a href="http://www.amazon.com/HTML-CSS-Design-Build-Websites/dp/1118008189%3FSubscriptionId%3DAKIAJYKJBAJTAUI7IXAQ%26tag%3Dcoursekit-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3D1118008189" target="_blank">Amazon</a>) is now available in the <a href="//innoserv.library.drexel.edu/search~S9?/tHTML+and+CSS:+Design+and+Build+Websites/thtml+and+css+design+and+build+websites/1%2C1%2C2%2CB/frameset&amp;FF=thtml+and+css+design+and+build+websites&amp;1%2C%2C2" target="_blank">Haggerty Library electronic resources</a>.</p>
+							<div id="div_chart">
+								<canvas id="canvas"></canvas>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -71,6 +74,24 @@
 	<!-- / #layout -->
 
 	<script src="js/core.min.js"></script>
+	<script>
+		function resize_canvas(){
+			canvas        = document.getElementById("canvas");
+			canvas.width  = $("#div_chart").width();
+			canvas.height = $("#div_chart").height();
+			days_in_quarter();
+		}
+		function days_in_quarter() {
+			var oneDay       = 24*60*60*1000;
+			var today        = new Date();
+			var startDate    = new Date(2014,03,01); //year,month,day of first class session (month - 1; jan = 0)
+			var endDate      = new Date(2014,05,13); //year,month,day of last class session (month - 1; jan = 0)
+			var daysPast     = Math.round(Math.abs((today.getTime() - startDate.getTime())/(oneDay)));
+			var daysFuture   = Math.round(Math.abs((endDate.getTime() - today.getTime())/(oneDay)));
+			var doughnutData = [{value: daysPast, color:"#9c182f"},{value: daysFuture, color:"#1f8dd6"}];
+			var myDoughnut   = new Chart(document.getElementById("canvas").getContext("2d")).Doughnut(doughnutData);
+		}
+	</script>
 
 </body>
 </html>
